3e14656aec6d7d4261cb4be3a133ac2310b343d1
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2021-01-25 12:06:10 -0500

    added button that will link to my translatingpage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,8 +5,9 @@ import './HomePage.css'
  const HomePage = () => {
     return (
         <div className="bg_image">
-            <div>
-                
+            <div className="container">
+                 <div className="sub-title">Translator</div>
+                 <button className="ui inverted primary button">TRY IT NOW</button>
             </div>
             
         </div>
