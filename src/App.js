ed94b3737752efcc2240b50e8ce50e0db21122fa
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2021-01-25 11:30:30 -0500

    started creating homepage to use translations

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import SearchBar from './components/SearchBar'
+import HomePage from './components/HomePage'
 import './App.css';
 import Image from './components/Image'
 
@@ -8,14 +9,15 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="title"><Image source="yoda.jpg" />What would Yoda say?</h1>
-      <SearchBar character="yoda"  />
-      <h1 className="title"><Image source="jar-jar-binks.jpg" /> What would Jar Jar Binks say? </h1>
-      <SearchBar character="gungan" />
-      <h1 className="title"><Image source="Vader.jpeg" />What would a Sith Lord say?</h1>
-      <SearchBar character="sith" />
+      <HomePage />
     </div>
   );
 }
 
 export default App;
+ /*<h1 className="title"><Image source="yoda.jpg" />What would Yoda say?</h1>
+      <SearchBar character="yoda"  />
+      <h1 className="title"><Image source="jar-jar-binks.jpg" /> What would Jar Jar Binks say? </h1>
+      <SearchBar character="gungan" />
+      <h1 className="title"><Image source="Vader.jpeg" />What would a Sith Lord say?</h1>
+      <SearchBar character="sith" />*/
---
+++ src/components/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './HomePage.css'
+
+
+ const HomePage = () => {
+    return (
+        <div className="bg_image">
+            <div>
+                
+            </div>
+            
+        </div>
+    )
+}
+
+export default HomePage
